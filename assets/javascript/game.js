ee22f66ed5390388be1ae5ff3e334835c2b671f9
P Reijerkerk <preijerkerk@gmail.com>
2018-08-06 20:29:12 -0400

    added first three variables and the means to hold the references for the variables to get displayed

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+var wins = 0;
+var guessesRemaining = 10;
+var guesses = [];
+
+var wins = document.getElementById("wins");
+var guessesRemaining = document.getElementById("guess-remaining");
+var guesses = document.getElementById("guesses");
