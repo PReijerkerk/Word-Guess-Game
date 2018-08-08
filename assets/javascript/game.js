b65a90acc361e883133ae9f980ad253534362ee4
P Reijerkerk <preijerkerk@gmail.com>
2018-08-07 20:20:30 -0400

    Added remaining variables I will need for this assignment. Created an object to hold my words as well as the array to call from.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,29 @@
+//Variables
+//----------------
+
 var wins = 0;
 var guessesRemaining = 10;
-var guesses = [];
+var lettersGuessed = [];
+var placeholderArray = [];
+var priorPlaceholderArray = [];
+var wordPlaceholder = [];
+var word= [];
+var wordString = "";
+var userInput = "";
 
-var wins = document.getElementById("wins");
-var guessesRemaining = document.getElementById("guess-remaining");
-var guesses = document.getElementById("guesses");
+//Object for Pac-Man Hangman
+
+var PacManNames = {
+    word1: ["B","L","I","N","K","Y"],
+    word2: ["P","I","N","K","Y"],
+    word3: ["I","N","K","Y"],
+    word4: ["C","L","Y","D","E"],
+    word5: ["P","A","C","M","A","N"]
+};
+
+//Array of Pac-Man Names created from object created above
+var wordArray = [PacManNames.word1, PacManNames.word2, PacManNames.word3, PacManNames.word4, PacManNames.word5];
+
+//Placeholder function to trigger start of game
+
+//Function to collect userKeyInput
