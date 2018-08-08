9ce596be773f82716553474f94dd77ca19aedd1a
P Reijerkerk <preijerkerk@gmail.com>
2018-08-07 21:55:30 -0400

    created an array to hold the word and fill it with underscores until it is guessed; created an array to track guesses and guesses remaining

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -25,6 +25,7 @@ var PacManNames = {
 var wordArray = [PacManNames.word1, PacManNames.word2, PacManNames.word3, PacManNames.word4, PacManNames.word5];
 
 //Placeholder function to trigger start of game
+createWord(wordArray);
 
 //Function to collect userKeyInput
 //----------------------------------
@@ -32,7 +33,7 @@ document.onkeyup = function(event) {
     console.log("This key was entered", event.key);
     var keyPress;
 
-    if (typeof event != "undefined") {
+    if (typeof (event.key) !="undefined") { 
         keyPress = event.keyCode;
 
         //convert user input key into an upper case string.
@@ -48,4 +49,66 @@ document.onkeyup = function(event) {
         keyPress = e.which;
     } return false;
     
+};
+
+//Functions
+
+//creates an Array with a randomly selected word from the Pac-Man wordArray
+function createWord(wordArray) {
+    word =wordArray[Math.floor(Math.random()* wordArray.length)];
+    console.log(word);
+
+//creates a placeholder array for the chosen word
+createWordPlaceholder(word);
+return word;
+};
+
+//defines the function for createWordPlaceholder
+function createWordPlaceholder(word) {
+    var wordPlaceholder = [];
+
+    //Fill wordPlaceholder Array with underscores
+    for (i = 0; i < word.length; i++) {
+        wordPlaceholder.push("_");
+    }
+
+    //convert array into a string for displaying on html
+    wordString = wordPlaceholder.join(" ");
+
+    //Displays word on page
+    document.getElementById("wordPlaceholder").textContent = wordString;
+    return wordPlaceholder;
+};
+
+//Track user letter guesses
+function trackLetterGuesses(userInput) {
+    //Checks if the letter is already guessed
+    //Tracks letters only once
+    for (i = 0; i <lettersGuessed.length; i++) {
+        if (userInput == lettersGuessed[i]) {
+            return;
+        }
+    }
+
+    //Push letters guessed into lettersGuessed
+    lettersGuessed.push(userInput);
+    console.log("letters guessed" + lettersGuessed[0]);
+
+    //convert the letters guessed into a string for displaying into the html
+    var lettersGuessedString = lettersGuessed.join(", ");
+    document.getElementById("guesses").innerHTML = lettersGuessedString;
+
+    //Reduces the number of guesses left
+    guessesRemaining--;
+
+    //display guesses left in html
+    document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
+    console.log("Guesses left" + guessesRemaining);
+
+    //Restart the game if guesses gets to 0
+    if (guessesRemaining == 0) {
+        restartGame();
+    }
+
+    return lettersGuessedString;
 };
